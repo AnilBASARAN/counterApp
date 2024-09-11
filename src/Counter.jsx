589804bf2031c1f06a6ec7c7708c110ed4d35aae
Anıl Başaran <nlbsrn@gmail.com>
2024-09-11 21:00:50 +0300

    get rid of the janky movement

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -11,7 +11,7 @@ const Counter =()=> {
 
         <div className="counterContainer flex flex-row">
             <button className="rounded-xl bg-green-400 m-4 p-4 text-3xl" onClick={plusHandler} >+</button>
-            <div className="m-4 p-4 text-3xl" >{counter}</div>
+            <div className="m-4 p-4 text-3xl w-20 text-center" >{counter}</div>
         <button className="rounded-xl bg-red-300 m-4 p-4 text-3xl" onClick={minusHandler} >-</button>
 
         </div>
